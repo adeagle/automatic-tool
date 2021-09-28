6f90fcb59949b95f3a9e1838b7fd7e69fb8d07d4
DESKTOP-FFQ7OSJ\jean <wujie@nj.iscas.ac.cn>
2021-09-28 16:12:42 +0800

    update

--- docker-ssh/run_debian.sh
+++ docker-ssh/run_debian.sh
@@ -2,5 +2,8 @@
 
 set -ex
 
-sshport='3000'
-docker run -dit -p ${sshport}:22 -v /root/docker_debian:/home/share/ --restart=always --name debian_ssh kr1510/debian_ssh_riscv:v1.1
+sshport="10000"
+useraccount="jiewu-plct"
+containername="ubuntu_${useraccount}"
+
+docker run -dit -p ${sshport}:22 -v /root/docker_debian:/home/share/ --restart=always --name ${containername} kr1510/debian_ssh_riscv:v1.1
--- docker-ssh/run_ubuntu.sh
+++ docker-ssh/run_ubuntu.sh
@@ -2,5 +2,8 @@
 
 set -ex
 
-sshport='2000'
-docker run -dit -p ${sshport}:22 -v /root/docker_ubuntu:/home/share/ --restart=always --name ubuntu_ssh kr1510/ubuntu_ssh_riscv:v1.1
+sshport="20000"
+useraccount="jiewu-plct"
+containername="ubuntu_${useraccount}"
+
+docker run -dit -p ${sshport}:22 -v /root/docker_ubuntu:/home/share/ --restart=always --name ${containername} kr1510/ubuntu_ssh_riscv:v1.1
