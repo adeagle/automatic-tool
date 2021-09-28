7f359ae3e08b13226eb4d5d98fea538846f8c5a5
DESKTOP-FFQ7OSJ\jean <wujie@nj.iscas.ac.cn>
2021-09-28 22:54:14 +0800

    update

--- get-pubkeys/get-user-pubkey.sh
+++ get-pubkeys/get-user-pubkey.sh
@@ -3,15 +3,15 @@
 set -ex
 
 
-useraccount="jiewu-plct"
-containername="ubuntu_${useraccount}"
+githubID="jiewu-plct"
+containername="ubuntu_${githubID}"
 
 
-if [ ! -d "${useraccount}_pubkey" ]; then
-  mkdir ${useraccount}_pubkey
+if [ ! -d "${githubID}_pubkey" ]; then
+  mkdir ${githubID}_pubkey
 fi
 
 
-curl https://github.com/${useraccount}.keys > ./${useraccount}_pubkey/id_rsa.pub
+curl https://github.com/${githubID}.keys > ./${githubID}_pubkey/id_rsa.pub
 
-docker cp ./${useraccount}_pubkey/id_rsa.pub ${containername}:/home/tester/.ssh/id_rsa.pub
+docker cp ./${githubID}_pubkey/id_rsa.pub ${containername}:/home/tester/.ssh/id_rsa.pub
