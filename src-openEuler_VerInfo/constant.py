b22bddecb5e447bc9b44f866cea7031e6f7cd693
DESKTOP-FFQ7OSJ\jean <wujie@nj.iscas.ac.cn>
2022-02-23 10:09:42 +0800

    add rpm information in advanced_riscv64

--- src-openEuler_VerInfo/constant.py
+++ src-openEuler_VerInfo/constant.py
@@ -4,21 +4,26 @@ import time
 
 token = 'a2e816edcac5b4936dc10cac80333b9e'
 obs_account = {'user':'jean9823', 'password':'qwert12345'}
+repolist = ['standard_riscv64', 'advanced_riscv64']
 branchlist = ['master', 'openEuler-22.03-LTS', 'openEuler-22.03-LTS-Next']
 report_header = [
     'No.',
     'Package',
-    'obs_repository',
-    'obs_commit_id',
-    'obs_status',
-    'gitee_master',
-    'gitee_openEuler-22.03-LTS',
-    'gitee_openEuler-22.03-LTS-Next',
+    'obs repository',
+    'obs commit id',
+    'obs standard_riscv64 status',
+    'obs advanced_riscv64 status',
+    'gitee master',
+    'gitee openEuler-22.03-LTS',
+    'gitee openEuler-22.03-LTS-Next',
     'Lastest Update',
     'Upgrade Priority',
-    'has rpm history',
-    'rpm version'
+    'has rpm history in standard_riscv64',
+    'rpm version in standard_riscv64',
+    'has rpm history in advanced_riscv64',
+    'rpm version in advanced_riscv64'
     ]
+
 
 
 headers = {
